d89f85c900785ca7e9794426a047ee999bfab7ec
chiccheung <chiccheung@outlook.com>
2021-01-30 21:56:45 +0800

    Update training_script.py

--- scripts/training_script.py
+++ scripts/training_script.py
@@ -1,12 +1,13 @@
 """
 Importing librairies
 """
-#Tensorflow/KERAS
+# Tensorflow/KERAS
 import tensorflow as tf
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.callbacks import ModelCheckpoint
 from tensorflow.python.keras.models import model_from_json
-from tensorflow.python.keras.layers import ZeroPadding3D, Dense, Activation,Conv3D,MaxPooling3D,AveragePooling3D,Flatten,Dropout
+from tensorflow.python.keras.layers import ZeroPadding3D, Dense, Activation, Conv3D, MaxPooling3D, AveragePooling3D, \
+    Flatten, Dropout
 from tensorflow.python.keras.utils import np_utils
 from tensorflow.python.keras.models import load_model
 
@@ -33,13 +34,13 @@ Setting of input data
 winSize = int(config['dataConfig']['winSize'])
 videoRate = int(config['dataConfig']['videoRate'])
 
-LENGTH_VIDEO = winSize * videoRate #frames
-IMAGE_WIDTH = int(config['dataConfig']['imageWidth']) #pixels
-IMAGE_HEIGHT = int(config['dataConfig']['imageHeight']) #pixels
-IMAGE_CHANNELS = int(config['dataConfig']['imageChannels']) #1 or 2 or 3
+LENGTH_VIDEO = winSize * videoRate  # frames
+IMAGE_WIDTH = int(config['dataConfig']['imageWidth'])  # pixels
+IMAGE_HEIGHT = int(config['dataConfig']['imageHeight'])  # pixels
+IMAGE_CHANNELS = int(config['dataConfig']['imageChannels'])  # 1 or 2 or 3
 
-#Time notion
-SAMPLING = 1 / videoRate #30 Hz
+# Time notion
+SAMPLING = 1 / videoRate  # 30 Hz
 t = np.linspace(0, LENGTH_VIDEO * SAMPLING - SAMPLING, LENGTH_VIDEO)
 
 print("Setting of input data.. done")
@@ -77,21 +78,19 @@ NB_SESSION = int(config['modelConfig']['nbSession'])
 
 CONTINUE_TRAINING = False
 if (int(config['modelConfig']['continueTraining']) == 1):
-    CONTINUE_TRAINING = True #load or not a old trained model
-
+    CONTINUE_TRAINING = True  # load or not a old trained model
 
 MIXED_DATA = False
 if (int(config['modelConfig']['mixedData']) == 1):
-    MIXED_DATA = True #use both types of datasets
-
+    MIXED_DATA = True  # use both types of datasets
 
 RESULTS_PATH = str(config['modelConfig']['modelPath'])
 
 REAL_VIDEO_DATASET = str(config['dataConfig']['realVideoDataset'])
 
-useCPU  = int(config['modelConfig']['useCPU'])
-if(useCPU == 1):
-    #RUN ON CPU 
+useCPU = int(config['modelConfig']['useCPU'])
+if (useCPU == 1):
+    # RUN ON CPU
     os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 print("Setting of training session.. done")
@@ -111,15 +110,16 @@ Definition of the 3D CNN model
 # DEFINE MODEL
 model = Sequential()
 
-#feature extraction
-model.add(Conv3D(filters=32, kernel_size=(LENGTH_VIDEO-2,IMAGE_HEIGHT-5,IMAGE_WIDTH-5), input_shape=(LENGTH_VIDEO, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)))
-model.add(MaxPooling3D(pool_size=(2,2,2)))
+# feature extraction
+model.add(Conv3D(filters=32, kernel_size=(LENGTH_VIDEO - 2, IMAGE_HEIGHT - 5, IMAGE_WIDTH - 5),
+                 input_shape=(LENGTH_VIDEO, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)))
+model.add(MaxPooling3D(pool_size=(2, 2, 2)))
 model.add(Activation('relu'))
 model.add(Dropout(0.2))
 
 model.add(Flatten())
 
-#Classification
+# Classification
 model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(NB_CLASSES + 1, activation='softmax'))
@@ -133,11 +133,10 @@ print("Definition of the 3D CNN model.. done")
 Trend generation parameters for artificial data creation
 """
 
-
 # Tendencies (linear, 2nd order, 3rd order)
-TENDANCIES_MIN = (-3,-1,-1)
-TENDANCIES_MAX = (3,1,1)
-TENDANCIES_ORDER = (1,2,3)
+TENDANCIES_MIN = (-3, -1, -1)
+TENDANCIES_MAX = (3, 1, 1)
+TENDANCIES_ORDER = (1, 2, 3)
 
 # coefficients for the fitted-ppg method
 a0 = 0.440240602542388
@@ -147,48 +146,51 @@ a2 = -0.050159136439220
 b2 = 0.099347477830878
 w = 2 * np.pi
 
-xtrain = np.zeros(shape=((NB_CLASSES + 1) * NB_VIDEOS_BY_CLASS_TRAIN, LENGTH_VIDEO, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS))
+xtrain = np.zeros(
+    shape=((NB_CLASSES + 1) * NB_VIDEOS_BY_CLASS_TRAIN, LENGTH_VIDEO, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS))
 ytrain = np.zeros(shape=((NB_CLASSES + 1) * NB_VIDEOS_BY_CLASS_TRAIN, NB_CLASSES + 1))
-
 
 """
 Trend generation function for artificial data creation
 """
-def generate_trend(length, order, min, max, offset):
 
-    if (order==1):   # linear
+
+def generate_trend(length, order, min, max, offset):
+    if (order == 1):  # linear
         tend = np.linspace(min, max, length)
 
-    elif (order==2): # quadratic
-        if (offset==0):
+    elif (order == 2):  # quadratic
+        if (offset == 0):
             tend = np.linspace(0, 1, length)
-            tend = tend*tend
-            tend = tend-min
-            tend = max*tend/np.max(tend)
+            tend = tend * tend
+            tend = tend - min
+            tend = max * tend / np.max(tend)
 
         else:
             tend = tend = np.linspace(-0.5, 0.5, length)
-            tend = tend*tend
-            tend = tend-min
-            tend = 0.5*max*tend/np.max(tend)
+            tend = tend * tend
+            tend = tend - min
+            tend = 0.5 * max * tend / np.max(tend)
 
-    elif (order==3): # cubic
-        if (offset==0):
+    elif (order == 3):  # cubic
+        if (offset == 0):
             tend = np.linspace(0, 1, length)
-            tend = tend*tend*tend
-            tend = tend-min
-            tend = max*tend/np.max(tend)
+            tend = tend * tend * tend
+            tend = tend - min
+            tend = max * tend / np.max(tend)
 
         else:
             tend = tend = np.linspace(-0.5, 0.5, length)
-            tend = tend*tend*tend
-            tend = tend-min
-            tend = 0.5*max*tend/np.max(tend)
+            tend = tend * tend * tend
+            tend = tend - min
+            tend = 0.5 * max * tend / np.max(tend)
     return tend
+
 
 """
 Train Data generation
 """
+
 
 def dataGeneration(xtrain, ytrain):
     print("start : Train Data generation ...")
@@ -199,29 +201,34 @@ def dataGeneration(xtrain, ytrain):
 
         for i_videos in range(NB_VIDEOS_BY_CLASS_TRAIN):
 
-            t2 = t + (np.random.randint(low=0, high=33) * SAMPLING)   # phase
-            signal = a0 + a1 * np.cos(t2 * w * HEART_RATES[i_freq] / 60) + b1 * np.sin(t2 * w * HEART_RATES[i_freq] / 60) + a2 * np.cos(2 * t2 * w * HEART_RATES[i_freq] / 60) + b2 * np.sin(2 * t2 * w * HEART_RATES[i_freq] / 60)
+            t2 = t + (np.random.randint(low=0, high=33) * SAMPLING)  # phase
+            signal = a0 + a1 * np.cos(t2 * w * HEART_RATES[i_freq] / 60) + b1 * np.sin(
+                t2 * w * HEART_RATES[i_freq] / 60) + a2 * np.cos(2 * t2 * w * HEART_RATES[i_freq] / 60) + b2 * np.sin(
+                2 * t2 * w * HEART_RATES[i_freq] / 60)
             signal = signal - np.min(signal)
             signal = signal / np.max(signal)
 
             r = np.random.randint(low=0, high=len(TENDANCIES_MAX))
-            trend = generate_trend(len(t), TENDANCIES_ORDER[r], 0, np.random.uniform(low=TENDANCIES_MIN[r], high=TENDANCIES_MAX[r]), np.random.randint(low=0, high=2))
+            trend = generate_trend(len(t), TENDANCIES_ORDER[r], 0,
+                                   np.random.uniform(low=TENDANCIES_MIN[r], high=TENDANCIES_MAX[r]),
+                                   np.random.randint(low=0, high=2))
 
             signal = np.expand_dims(signal + trend, 1)
             signal = signal - np.min(signal)
 
             img = np.tile(signal, (IMAGE_WIDTH, 1, IMAGE_HEIGHT))
-            img = np.transpose(img, axes=(0,2,1))
+            img = np.transpose(img, axes=(0, 2, 1))
 
             img = img / (IMAGE_HEIGHT * IMAGE_WIDTH)
-        
+
             amplitude = np.random.uniform(low=1.5, high=4)
             noise_energy = amplitude * 0.25 * np.random.uniform(low=1, high=10) / 100
 
             for j in range(0, LENGTH_VIDEO):
-                temp = 255 * ((amplitude * img[:,:,j]) + np.random.normal(size=(IMAGE_HEIGHT, IMAGE_WIDTH), loc=0.5, scale=0.25) * noise_energy)
-                temp[temp < 0] = 0 
-                xtrain[c,j,:,:,0] = temp.astype('uint8') / 255.0
+                temp = 255 * ((amplitude * img[:, :, j]) + np.random.normal(size=(IMAGE_HEIGHT, IMAGE_WIDTH), loc=0.5,
+                                                                            scale=0.25) * noise_energy)
+                temp[temp < 0] = 0
+                xtrain[c, j, :, :, 0] = temp.astype('uint8') / 255.0
 
             xtrain[c] = xtrain[c] - np.mean(xtrain[c])
             ytrain[c] = labels_cat[i_freq]
@@ -231,19 +238,22 @@ def dataGeneration(xtrain, ytrain):
     # constant image noise (gaussian distribution)
     for i_videos in range(NB_VIDEOS_BY_CLASS_TRAIN):
         r = np.random.randint(low=0, high=len(TENDANCIES_MAX))
-        trend = generate_trend(len(t), TENDANCIES_ORDER[r], 0, np.random.uniform(low=TENDANCIES_MIN[r], high=TENDANCIES_MAX[r]), np.random.randint(low=0, high=2))
+        trend = generate_trend(len(t), TENDANCIES_ORDER[r], 0,
+                               np.random.uniform(low=TENDANCIES_MIN[r], high=TENDANCIES_MAX[r]),
+                               np.random.randint(low=0, high=2))
 
         # add a tendancy on noise
         signal = np.expand_dims(trend, 1)
         img = np.tile(signal, (IMAGE_WIDTH, 1, IMAGE_HEIGHT)) / (IMAGE_HEIGHT * IMAGE_WIDTH)
-        img = np.expand_dims(np.transpose(img, axes=(1,0,2)), 3)
+        img = np.expand_dims(np.transpose(img, axes=(1, 0, 2)), 3)
 
         xtrain[c] = np.expand_dims(np.random.normal(size=(LENGTH_VIDEO, IMAGE_HEIGHT, IMAGE_WIDTH)) / 50, 3) + img
         xtrain[c] = xtrain[c] - np.mean(xtrain[c])
         ytrain[c] = labels_cat[NB_CLASSES]
         c = c + 1
     print('Train data generation .. done')
-    return xtrain , ytrain
+    return xtrain, ytrain
+
 
 """
 Start a training session + save
@@ -262,20 +272,20 @@ for serie in range(NB_SESSION):
     print("Session " + str(serie))
     xtrain, ytrain = dataGeneration(xtrain, ytrain)
 
-    if(MIXED_DATA == True):
+    if (MIXED_DATA == True):
         data = np.load(REAL_VIDEO_DATASET)
         xtrain = np.concatenate((xtrain, data['a']), axis=0)
-        ytrain =  np.concatenate((ytrain, data['b']), axis=0)
+        ytrain = np.concatenate((ytrain, data['b']), axis=0)
 
         indices = np.arange(xtrain.shape[0])
         np.random.shuffle(indices)
         xtrain = xtrain[indices]
         ytrain = ytrain[indices]
-    
-    #start training
-    model.fit(xtrain, ytrain, epochs = EPOCHS, batch_size=BATCH_SIZE, verbose=VERBOSE)
 
-    #save data
+    # start training
+    model.fit(xtrain, ytrain, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=VERBOSE)
+
+    # save data
     model_json = model.to_json()
     open(model_name, 'w').write(model_json)
     model.save_weights(weights_name, overwrite=True)
