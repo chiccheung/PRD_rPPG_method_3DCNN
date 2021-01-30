c28739fcb713927c3512382d60f2b4729236addc
chiccheung <chiccheung@outlook.com>
2021-01-30 21:57:40 +0800

    Update my_demo1.py

--- scripts/my_demo1.py
+++ scripts/my_demo1.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 
 # -- Modules and packages to import for demo
 from pyVHR.signals.video import Video
@@ -6,7 +6,7 @@ from pyVHR.methods.pos import POS
 from pyVHR.methods.chrom import CHROM
 from pyVHR.analysis.testsuite import TestSuite, TestResult
 
-#%%
+# %%
 
 # -- Video object
 videoFilename = "./alex_resting/cv_camera_sensor_stream_handler.avi"
@@ -19,36 +19,35 @@ video.printVideoInfo()
 print("\nShow video cropped faces, crop size:", video.cropSize)
 video.showVideo()
 
-
-#%%
+# %%
 
 # -- define ROIs: free rectangular regions
-video.setMask(typeROI='rect', rectCoords=[[15,20,140,50],[10,120,100,30]])
+video.setMask(typeROI='rect', rectCoords=[[15, 20, 140, 50], [10, 120, 100, 30]])
 video.printROIInfo()
 video.showVideo()
 
-#%%
+# %%
 
 # -- define ROIs: standard regions, i.e. 'forehead', 'lcheek', 'rcheek', 'nose'
 video.setMask(typeROI='rect', rectRegions=['forehead', 'lcheek', 'rcheek', 'nose'])
 video.printROIInfo()
 video.showVideo()
 
-#%%
+# %%
 
 # -- define ROIs: using skin, with threshold param
-video.setMask(typeROI='skin_adapt',skinThresh_adapt=0.2)
+video.setMask(typeROI='skin_adapt', skinThresh_adapt=0.2)
 video.printROIInfo()
 video.showVideo()
 
-#%%
+# %%
 
 # -- define ROIs: using skin, with threshold param
-video.setMask(typeROI='skin_fix',skinThresh_fix=[20, 50])
+video.setMask(typeROI='skin_fix', skinThresh_fix=[20, 50])
 video.printROIInfo()
 video.showVideo()
 
-#%%
+# %%
 
 # -- Define a configuration file.
 #    It contains all the information relative to the dataset (e.g. the path),
@@ -68,16 +67,16 @@ test = TestSuite(configFilename=cfgFilename)
 #       (use also combinations, e.g. verb=21, verb=321)
 result = test.start(outFilename='sampleExp.h5', verb=1)
 
-#%%
+# %%
 
 # -- Detailed pipeline for rafinement steps and tuning
 
 # -- define some params in the form of dict (those in the cfg file)
-params = {"video": video, "verb":2, "ROImask":"skin_adapt", "skinAdapt":0.2}
+params = {"video": video, "verb": 2, "ROImask": "skin_adapt", "skinAdapt": 0.2}
 
 # -- invoke the method
 m = CHROM(**params)
-#m = POS(**params)
+# m = POS(**params)
 
 # -- invoke the method
 bpmES, timesES = m.runOffline(**params)
